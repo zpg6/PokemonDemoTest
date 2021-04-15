511525502e30fba7e8db0c10756f23dc6b73d16d
Zach <zpg6@pitt.edu>
2021-04-15 16:25:04 -0400

    Initial Commit

---
+++ PokemonDemoTest/PokemonDemoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonDemoTestApp.swift
+//  PokemonDemoTest
+//
+//  Created by Zachary Grimaldi on 4/15/21.
+//
+
+import SwiftUI
+
+@main
+struct PokemonDemoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
